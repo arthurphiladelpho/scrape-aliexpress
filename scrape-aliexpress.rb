b4e53021750f6bfe3b75d994fcff849cdbc90efd
Arthur Philadelpho <arthur22rj@gmail.com>
2018-10-15 20:27:34 -0300

    Scraper works.

--- scrape-aliexpress.rb
+++ scrape-aliexpress.rb
@@ -1,8 +1,8 @@
 require 'open-uri'
 require 'nokogiri'
+require 'csv'
 
-# The scrape method:
-# will grab html content and convert it into a Nokogiri HTML Document object.
+# Grab html content and convert it into a Nokogiri HTML Document object.
 def scrape(url)
   # Grab html string.
   html_string = open(url).read
@@ -11,9 +11,7 @@ def scrape(url)
   return nokogiri_html_doc
 end
 
-# The scrape_urls method:
-# Given an Aliexpress products result page,
-# it will grab all the product's urls.
+# Given an Aliexpress products result page, grab all the product's urls.
 def scrape_urls(nokogiri_html_doc)
   # Create an array of urls.
   urls = []
@@ -23,14 +21,6 @@ def scrape_urls(nokogiri_html_doc)
   end
   return urls
 end
-
-# Grab a list of urls.
-belts_url = "https://pt.aliexpress.com/category/201005182/yoga-belts.html?site=bra&g=y&needQuery=n&isrefine=y"
-belts_html_doc = scrape(belts_url)
-belts_urls = scrape_urls(belts_html_doc)
-# Loop over array of urls and run method to grab the data we want.
-
-single_product_url = "https://pt.aliexpress.com/item/New-Arrival-Fitness-Exercise-Gym-Yoga-Stretch-Strap-Belt-Figure-Waist-Leg-Yoga-Stretch-SA785-P40/32781467407.html?spm=a2g03.search0103.3.1.17a61a1emjOfgy&ws_ab_test=searchweb0_0,searchweb201602_2_10065_10068_318_10547_319_5727315_10548_10696_450_10084_10083_10618_452_535_534_533_10307_532_5727215_204_10059_10884_10887_100031_320_10103_448_449,searchweb201603_60,ppcSwitch_0&algo_expid=9b496794-5bfa-4067-916d-7dbaaa1b1169-0&algo_pvid=9b496794-5bfa-4067-916d-7dbaaa1b1169&transAbTest=ae803_4&priceBeautifyAB=0"
 
 # Go into each url and grab essential information.
 def scrape_product(url)
@@ -45,7 +35,11 @@ def scrape_product(url)
   # Search html object for the orders.
   nokogiri_html_doc.search('span.order-num').each do |element|
     orders_count = element.text.strip
-    orders_count.slice!(" pedidos")
+    if orders_count[-1] == 'o'
+      orders_count.slice!(" pedido")
+    else
+      orders_count.slice!(" pedidos")
+    end
     orders_count = Integer(orders_count)
   end
   # Search html object for the rating.
@@ -69,12 +63,68 @@ def scrape_product(url)
   return product
 end
 
-# products = belts_urls.each_with_index { |item, index|
-#   p scrape_product(item)
-#   break
-# }
-p scrape_product(single_product_url)
+# Grab a list of urls.
+def get_list_of_urls(url)
+  # belts_url = "https://pt.aliexpress.com/category/201005182/yoga-belts.html?site=bra&g=y&needQuery=n&isrefine=y"
+  html_doc = scrape(url)
+  arr_of_urls = scrape_urls(html_doc)
+  arr_of_urls.map! do |url|
+    'https:' + url
+  end
+end
 
+# Get information about a bunch of products.
+def scrape_list_of_products(list, results)
+  list.each do |item|
+    values = []
+    item = scrape_product(item)
+    item.each do |key, value|
+      values << value
+    end
+    results << values
+  end
+end
 
-# Next up:
-# votes_count
+# Export products to .csv file.
+def write_csv(csv_file_name, products)
+  csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+  CSV.open(csv_file_name, 'wb', csv_options) do |csv|
+    csv << ['Title', 'Orders Count', 'Rating', 'Votes Count']
+    products.each do |values|
+      csv << values
+    end
+  end
+end
+
+# Grab and export products to .csv file.
+def get_products(url, csv_file_name)
+  url_list = get_list_of_urls(url)
+  product_list = Array.new
+  scrape_list_of_products(url_list, product_list)
+  write_csv(csv_file_name, product_list)
+  p csv_file_name + ' exported successfully.'
+end
+
+urls_and_csvs = [
+  # belts
+  ['https://pt.aliexpress.com/category/201005182/yoga-belts.html?isrefine=y&site=bra&g=y&needQuery=n&tag=', 'belts.csv'],
+  ['https://pt.aliexpress.com/category/201005182/yoga-belts/2.html?isrefine=y&site=bra&g=y&needQuery=n&tag=', 'belts.csv'],
+  # blocks
+  ['https://pt.aliexpress.com/category/201005181/yoga-blocks.html?site=bra&g=y&needQuery=n&isrefine=y', 'blocks.csv'],
+  ['https://pt.aliexpress.com/category/201005181/yoga-blocks/2.html?isrefine=y&site=bra&g=y&needQuery=n&tag=', 'blocks.csv'],
+  ['https://pt.aliexpress.com/category/201005181/yoga-blocks/3.html?isrefine=y&site=bra&g=y&needQuery=n&tag=', 'blocks.csv'],
+  # mats
+  ['https://pt.aliexpress.com/category/201005176/yoga-mats.html?site=bra&g=y&needQuery=n&isrefine=y', 'mats.csv'],
+  ['https://pt.aliexpress.com/category/201005176/yoga-mats/2.html?isrefine=y&site=bra&g=y&needQuery=n&tag=', 'mats.csv'],
+  # blankets
+  ['https://pt.aliexpress.com/category/201005184/yoga-blankets.html?isrefine=y&site=bra&g=y&needQuery=n&tag=', 'blankets.csv'],
+  # circles
+  ['https://pt.aliexpress.com/category/201005179/yoga-circles.html?site=bra&g=y&needQuery=n&tag=', 'circles.csv'],
+  ['https://pt.aliexpress.com/category/201005179/yoga-circles/2.html?site=bra&g=y&needQuery=n&tag=', 'circles.csv']
+]
+
+urls_and_csvs.each do |page|
+  get_products(page[0], page[1])
+end
+
+puts 'Scrape was successful.'
